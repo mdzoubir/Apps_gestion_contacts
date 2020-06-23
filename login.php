791647e51400fd19f178366d6cbe37a8dd641cd9
mdzoubir <57962405+mdzoubir@users.noreply.github.com>
2020-06-23 23:49:38 +0100

    ajouter les fichier

---
+++ login.php
@@ -0,0 +1,41 @@
+<?php
+include('connect.php')
+
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/accueil.css">
+</head>
+<body>
+    <div class="loginup">
+        <header>
+            <h2>Contacts list</h2>
+            <ul class="log">
+                <h3>
+                    <a href="login.php">Login</a>
+                </h3>
+            </ul>
+        </header>
+        <h1 id="authe">Authenticate</h1>
+        <form action="" method="POST" class="login">
+            <label class="lab">Username</label>
+            <input type="text" placeholder="Username" name="User" class="put">
+            <label class="lab">Password</label>
+            <input type="password" placeholder="password" name="pass"  class="put">
+            <input type="submit" name="login" value="Login">
+        </form>
+        <p class="No">No account? <a href="#">Sign up</a> here.</p>
+    </div>
+</body>
+</html>
