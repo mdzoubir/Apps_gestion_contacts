86c6b86a2a45cc0403563e7417c6cb92df38074a
mdzoubir <57962405+mdzoubir@users.noreply.github.com>
2020-07-06 00:38:33 +0100

    add page contact

--- contact.php
+++ contact.php
@@ -12,17 +12,14 @@ if(isset($_POST['envey'])){
         Phone : $phone \n
         Email :  $email \n
         Message : $message";
-        $entete =  'MIME-Version: 1.0' . "\r\n"; 
-        $entete .= "From : $nom <$email>" . "\r\n";
-        $entete .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
-        mail($destinataire, $sujet, $msg, $entete);
+        $headers =  'MIME-Version: 1.0' . "\r\n"; 
+        $headers .= "From: $nom <$email>" . "\r\n";
+        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";  
+        mail($destinataire, $sujet, $msg, $headers);
     }else{
         $erreur= 'tous les champs doivent étre complétés !';
     }
 }
-
-
-
 
 
 
@@ -34,23 +31,27 @@ if(isset($_POST['envey'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>page contact</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
+    
 </head>
 <body>
     <?php include('navbar.php');?>
     <div class="contact">
         <form action="" method="POST">
-            <label for="">Name</label>
-            <input type="text" placeholder="Enter name"name="name">
-            <label for="">Phone</label>
-            <input type="text" placeholder="Phone" name="phone">
+            <div class="nameCon">
+                <label for="" class="frlabel">Name</label>
+                <input type="text" placeholder="Enter name"name="name">
+                <label for="" class="seclabel">Phone</label>
+                <input type="text" placeholder="Phone" name="phone">
+            </div>
             <label for="">Email</label>
             <input type="email" placeholder="Email" name="email">
             <label for="">Message</label>
             <textarea name="msg" id="" cols="30" rows="10" placeholder="message"></textarea>
             <input type="submit" name="envey" value="Envey">
         </form>
-        <?php if(isset($erreur)){ echo $erreur;} ?>
+        
     </div>
+    <?php if(isset($erreur)){ echo $erreur;} ?>
 </body>
 </html>
